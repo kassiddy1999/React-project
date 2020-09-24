bd2d17b565c8242e6bbb9f33b89831a1bed52aef
kassidy10 <victoriheanachoc@gmail.com>
2020-09-24 17:07:55 +0100

    commiting files

--- mosh/src/App.js
+++ mosh/src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React , { Component } from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 
-function App() {
+class App extends Component {
+  state = {
+    todos : [
+      { id : 1, content : 'buy some milk'},
+      { id : 2, content : 'go to the market'},
+      {id : 3, content : 'go shopping'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos  = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos : todos
+    })
+  }
+  addTodo = (todo) =>{
+      todo.id = Math.random()
+      let todos = [...this.state.todos, todo]
+      this.setState({
+        todos : todos
+      })
+
+  }
+render(){
   return (
-    <div className="App">
-    <h1> Hello world! </h1>
+    <div className ="App" className = "container">
+    <h1 className = "center blue-text"> Todo's</h1>
+    <Todos todos = { this.state.todos } deleteTodo = { this.deleteTodo }/>
+    <AddTodo addTodo = { this.addTodo }/>
     </div>
   );
+}
 }
 
 export default App;
